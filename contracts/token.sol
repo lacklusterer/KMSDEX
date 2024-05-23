fdfc60336fb9f67cfe04ace8f1271262eddcaa95
scev <minh.nn210582@sis.hust.edu.vn>
2024-05-23 15:46:40 +0700

    Remove unneeded files, implemented addLiquidity()

--- contracts/token.sol
+++ contracts/token.sol
@@ -6,30 +6,26 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 
 // Your token contract
 contract Token is Ownable, ERC20 {
-    string private constant _symbol = ""; // TODO: Give your token a symbol (all caps!)
-    string private constant _name = ""; // TODO: Give your token a name
+	string private constant _symbol = "KMS";
+	string private constant _name = "KeepMyStuff";
 
-    // TODO: add private members as needed!
-    // TODO: if you create private member, initialize it in the constructor
+	bool private mint_enabled = true;
 
-    constructor() ERC20(_name, _symbol) {}
+	constructor() ERC20(_name, _symbol) {}
 
-    // ============================================================
-    //                    FUNCTIONS TO IMPLEMENT
-    // ============================================================
+	// Function mint: Create more of your tokens.
+	// You can change the inputs, or the scope of your function, as needed.
+	// Do not remove the onlyOwner modifier!
+	function mint(uint amount) public onlyOwner {
+		require(mint_enabled, "Minting disabled");
+		_mint(msg.sender, amount);
+	}
 
-    // Function mint: Create more of your tokens.
-    // You can change the inputs, or the scope of your function, as needed.
-    // Do not remove the onlyOwner modifier!
-    function mint(uint amount) public onlyOwner {
-        /******* TODO: Implement this function *******/
-    }
-
-    // Function disable_mint: Disable future minting of your token.
-    // You can change the inputs, or the scope of your function, as needed.
-    // Do not remove the onlyOwner modifier!
-    function disable_mint() public onlyOwner {
-        /******* TODO: Implement this function *******/
-    }
+	// Function disable_mint: Disable future minting of your token.
+	// You can change the inputs, or the scope of your function, as needed.
+	// Do not remove the onlyOwner modifier!
+	function disable_mint() public onlyOwner {
+		require(mint_enabled, "Minting already disabled");
+		mint_enabled = false;
+	}
 }
-
--- make_submit.sh
+++
@@ -1,2 +0,0 @@
-rm -f submission.zip
-zip -r submission.zip contracts/exchange.sol contracts/token.sol DesignDoc.txt web_app/exchange.js
