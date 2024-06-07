74ba90d3a90aebbfdaf772122a10039c7e17ab7f
scev <minh.nn210582@sis.hust.edu.vn>
2024-06-07 10:41:17 +0700

    HOTFIX: deploy script for verifier, reformatted old script

--- scripts/deploy_zk.js
+++ scripts/deploy_zk.js
@@ -2,20 +2,20 @@ const hre = require("hardhat");
 const fs = require("fs");
 
 async function main() {
-	const TokenContract = await hre.ethers.getContractFactory("Token");
-	const tokenContract = await TokenContract.deploy();
-	await tokenContract.deployed();
+	const VerifierContract = await hre.ethers.getContractFactory("Verifier");
+	const verifierContract = await VerifierContract.deploy();
+	await verifierContract.deployed();
 
 	const envFilePath = "./.env";
-	const newAddress = `TOKEN_ADDRESS=${tokenContract.address}`;
+	const newAddress = `VERIFIER_ADDRESS=${verifierContract.address}`;
 
 	try {
 		// Read the existing .env file
 		let envContent = fs.readFileSync(envFilePath, "utf8");
 
-		// Update the TOKEN_ADDRESS value
+		// Update the verifier_ADDRESS value
 		const updatedEnvContent = envContent.replace(
-			/TOKEN_ADDRESS=.*/g,
+			/VERIFIER_ADDRESS=.*/g,
 			newAddress
 		);
 
@@ -23,11 +23,11 @@ async function main() {
 		fs.writeFileSync(envFilePath, updatedEnvContent);
 
 		console.log(
-			`Successfully wrote token address ${tokenContract.address} to ${envFilePath}`
+			`Successfully wrote verifier address ${verifierContract.address} to ${envFilePath}`
 		);
 	} catch (error) {
 		console.log(`Failed to write to file`);
-		console.log(`Manually input token address: ${tokenContract.address}`);
+		console.log(`Manually input verifier address: ${verifierContract.address}`);
 	}
 }
 
